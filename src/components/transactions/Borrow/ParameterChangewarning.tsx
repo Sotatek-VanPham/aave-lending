47f428dc3d89d1b91251eedcda1b5653d6bafc8d
vanptn <van.pham@sotatek.com>
2024-03-26 14:48:33 +0700

    fix 106

--- src/components/transactions/Borrow/ParameterChangewarning.tsx
+++ src/components/transactions/Borrow/ParameterChangewarning.tsx
@@ -1,16 +1,17 @@
 import { Trans } from '@lingui/macro';
 import { Warning } from 'src/components/primitives/Warning';
-import { useRootStore } from 'src/store/root';
-import { GENERAL } from 'src/utils/mixPanelEvents';
+// import { useRootStore } from 'src/store/root';
+// import { GENERAL } from 'src/utils/mixPanelEvents';
 
-export const ParameterChangewarning = ({ underlyingAsset }: { underlyingAsset: string }) => {
-  const trackEvent = useRootStore((store) => store.trackEvent);
+export const ParameterChangewarning = ({ }: { underlyingAsset: string }) => {
+  // const trackEvent = useRootStore((store) => store.trackEvent);
 
   return (
     <Warning severity="info" sx={{ my: 6 }}>
       <Trans>
         <b>Attention:</b> Parameter changes via governance can alter your account health factor and
-        risk of liquidation. Follow the{' '}
+        risk of liquidation.
+         {/* Follow the{' '}
         <a
           onClick={() => {
             trackEvent(GENERAL.EXTERNAL_LINK, {
@@ -22,7 +23,7 @@ export const ParameterChangewarning = ({ underlyingAsset }: { underlyingAsset: s
         >
           Community Discord
         </a>{' '}
-        for updates.
+        for updates. */}
       </Trans>
     </Warning>
   );
